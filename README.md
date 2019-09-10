2bfb946fdc353f3940ed8e019f1d8dddc251b725
Everett Green <mrevrgreen@gmail.com>
2019-09-10 17:33:26 -0400

    added main code file an example sentence to the repo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # graph-parser
 graph-based parser
 
-To clone run the following command in your terminal once you are in your target folder:
+To clone, run the following command in your terminal once you are in your target folder:
 git clone https://github.com/MrEvrgreen/graph-parser.git
+
+Will use conll format and output each word tagged with part of speech and the root in the correctly formatted columns.
---
+++ graph_parser.py
@@ -0,0 +1,32 @@
+"""
+Graph-based Parser
+Authors: Ata, Everett, Kazuki
+"""
+
+"""
+// Tagger from the 500 line parser article
+
+class PerceptronTagger(object):
+    '''Greedy Averaged Perceptron tagger'''
+    model_loc = os.path.join(os.path.dirname(__file__), 'tagger.pickle')
+    def __init__(self, classes=None, load=True):
+        self.tagdict = {}
+        if classes:
+            self.classes = classes
+        else:
+            self.classes = set()
+        self.model = Perceptron(self.classes)
+        if load:
+            self.load(PerceptronTagger.model_loc)
+"""
+
+if __name__ == "__main__":
+    
+    // opens sample file
+    with open('example_parser_in_out.txt') as data:
+        file = data.readlines()
+    
+    // prints each line of the sample file
+    for i in range(len(file)):
+        print(file[i])
+
