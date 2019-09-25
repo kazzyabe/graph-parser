ffc886ffbef3818ede4650874b11f63c34e8b9a8
Kazuki Yabe <kyabe@iu.edu>
2019-09-24 22:06:49 -0400

    there is a bug in contraction

--- MaximumSpanning.py
+++ MaximumSpanning.py
@@ -1,3 +1,42 @@
+def adjacentT(V, E):
+    adjacent = {}
+    for v in V:
+        adjacent[v] = []
+    
+    # print(adjacent)
+    for e in E:
+        if e[0] in V and e[1] in V:
+            # print(e[0])
+            adjacent[e[0]] += [e[1]]
+
+    return adjacent
+
+def weight(E):
+    w = {}
+    for e in E:
+        w[(e[0],e[1])] = e[2]
+    return w
+
+def sumWeight(C, E):
+    w = weight(E)
+    sumW = 0
+    for c in C:
+        for c2 in C:
+            if (c,c2) in w:
+                sumW += w[(c,c2)]
+    return sumW
+
+def predecessor(c, V, E):
+    """
+    return predecessor p in V of c
+    c: current node
+
+    """
+    adjacent = adjacentT(V,E)
+    for v in V:
+        if c in adjacent[v]:
+            return v
+
 def cyclehelp(a, visited, adjacent):
     """
     detcycle helper
@@ -12,7 +51,7 @@ def cyclehelp(a, visited, adjacent):
     # print(a,visited,adjacent)
     if a in visited:
         # print("CYCLE: ", visited)
-        visited += [a]
+        # visited += [a]
         return visited
     elif adjacent[a] == []:
         # print("NO ADJACENT")
@@ -37,13 +76,17 @@ def detcycle(V,E):
     otherwise, return False
     """
     # creating adjacent list
-    adjacent = {}
-    for v in V[1:]:
-        adjacent[v] = []
+    # adjacent = {}
+    # for v in V[1:]:
+    #     adjacent[v] = []
 
-    for e in E:
-        adjacent[e[0]] += [e[1]]
+    # for e in E:
+    #     print(e)
+    #     adjacent[e[0]] += [e[1]]
     # print(adjacent)
+    # print("function: ")
+    # print(adjacent(V,E))
+    adjacent = adjacentT(V,E)
 
     # Cycle detection by DFS
     res = []
@@ -56,6 +99,47 @@ def detcycle(V,E):
         return True, res
     else:
         return False, []
+
+def contraction(V,E,C):
+    """
+    Contraction procedure in Chi-Liu-Edmonds
+    V: verticies
+    E: edges with weights
+    C: a cycle
+
+    1.Excluding vertices in C from V
+    2.Add a node c to V to represent the cycle
+    3.For v in V-C, if (a, v) in E for a in C
+        add (c,v) to E with weight max(y in C)weight(y,v)
+    4.For v in V-C, if (v,a) in E for a in C
+        add (v,c) to E with weight max(y in C)[W(v,y)-W(a(y),y) + W(C)]
+        where a(y) is the predecessor of y in C
+        W(C) is sum of all weights in C
+    """
+    adjacent = adjacentT(V,E)
+    # print(adjacent)
+    w = weight(E)
+    for c in C:
+        V.remove(c)
+    
+    for v in V:
+        for c in C:
+            if v in adjacent[c]:
+                maxW = 0
+                for c2 in C:
+                    if maxW < w[(c,v)]:
+                        maxW = w[(c,v)]
+                E += [["c",v, maxW]]
+            elif c in adjacent[v]:
+                maxW = 0
+                for c2 in C:
+                    tmp = w[v,c2] - w[predecessor(c2, C, E),c2] + sumWeight(C,E)
+                    if maxW < tmp:
+                        maxW = tmp
+                E += [[v,"c",maxW]]
+    V += ["c"]
+    return V,E
+                
 
 def maxspan(V,E):
     """
@@ -80,7 +164,9 @@ def maxspan(V,E):
     # if there are cycles
     if c:
         # continue
-        print(c,cycles)
+        print(cycles)
+        newV, newE = contraction(V,E,cycles[0])
+        print(adjacentT(newV,newE))
     return M
 
 if __name__ == "__main__":
@@ -90,7 +176,7 @@ if __name__ == "__main__":
         [0,2,10],
         [0,3,9],
         [1,2,20],
-        [1,2,3],
+        [1,3,3],
         [2,1,30],
         [2,3,30],
         [3,1,11],
