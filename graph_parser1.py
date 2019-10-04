e8c7ac2e7de0e101f0faaa66fc7a31869d98b384
Kazuki Yabe <kyabe@iu.edu>
2019-10-04 15:28:21 -0400

    Added POS extraction

--- graph_parser1.py
+++ graph_parser1.py
@@ -7,14 +7,17 @@ class graph_based_parser:
         where 
         V = verticies for each training data eg. V[0] = verticies for the first sentence
         E = edges for each training data eg. E[0] = edges for the first sentence
+        POS = universal part of speech tag
         '''
         f = open(f, "r")
         # Gold standard graphs in training eg V[0], E[0] = first graph
         V = []
         E = []
+        POS = []
         # tmp storage for v and e
         v_tmp = []
         e_tmp = []
+        pos_tmp = []
 
         flag = False
         l = f.readline()
@@ -28,20 +31,25 @@ class graph_based_parser:
                 V.append(v_tmp)
                 e_tmp.sort()
                 E.append(e_tmp)
+                POS.append(pos_tmp)
                 v_tmp = []
                 e_tmp = []
+                pos_tmp = []
             elif flag == True:
                 l = l.split("\t")
                 v_tmp.append(int(l[0]))
                 e_tmp.append([int(l[6]),int(l[0]),l[7]])
+                pos_tmp.append(l[3])
             l = f.readline()
-        return V, E
+        return V, E, POS
 
 if __name__ == "__main__":
     gparse = graph_based_parser()
-    V, E = gparse.load_data("/Users/kazuyabe/Documents/IU/courses/Codes/Python3/L545/01_Tokenisation/UD_Japanese-GSD-master/ja_gsd-ud-train.conllu")
+    V, E, POS = gparse.load_data("/Users/kazuyabe/Documents/IU/courses/Codes/Python3/L545/01_Tokenisation/UD_Japanese-GSD-master/ja_gsd-ud-train.conllu")
 
     print("V = ")
     print(V[55])
     print("E = ")
     print(E[55])
+    print("POS = ")
+    print(POS[55])
